7b0942c904520061e0a897edadb994b4fb8fc7ec
jeffprakash <jeffprakash@gmail.com>
2022-12-17 16:30:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import {Map,GoogleApiWrapper} from 'google-maps-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MapContainer extends Component {
+
+ render(){
+  
+    return(
+      <Map
+         google = {this.props.google}
+         style = {{width:"100%",height:"100%"}}
+         zoom = {12}
+         initialCenter= {
+          {
+            lat:10.0261,
+            lng:76.3125
+          }
+         }
+      
+      
+      
+      />
+
+
+
+    )
+
+  
+ }
+
 }
 
-export default App;
+export default GoogleApiWrapper({
+ apikey:"AIzaSyDRJvu5W7wye_HEgvtEaGDEVzlaQU0SWhg"  
+
+})(MapContainer)
+
+
